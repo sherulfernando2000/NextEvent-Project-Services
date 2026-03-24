f031d10910674deaefaf004e8b54f60dd0a03b21
Developer <developer@example.com>
2026-03-24 12:55:37 +0530

    added 2 instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,16 +4,19 @@ module.exports = {
       name   : "service-event",
       script : "java -jar ./service-event/target/Event-Service-1.0.0.jar",
       log_file:  "./logs/service-event.log",
+      instances: 2
     },
     {
       name   : "service-participant",
       script : "java -jar ./service-registration/target/Participant-Service-1.0.0.jar",
       log_file:  "./logs/service-participant.log",
+      instances: 2
     },
     {
       name   : "service-registration",
       script : "java -jar ./service-registration/target/Registration-Service-1.0.0.jar",
       log_file:  "./logs/service-registration.log",
+      instances: 2
     }
   ]
 }
