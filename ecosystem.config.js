8f272a768b609c12aa12223881b816c0e9a2fd28
Developer <developer@example.com>
2026-03-24 14:48:42 +0530

    added 2 instances and change ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,51 @@
 module.exports = {
   apps : [
     {
-      name   : "service-event",
-      script : "java -jar ./service-event/target/Event-Service-1.0.0.jar",
-      log_file:  "./logs/service-event.log",
-      instances: 2
+      name: "service-event",
+      script: "java",
+      args: "-jar ./service-event/target/Event-Service-1.0.0.jar",
+      instances: 2,
+      exec_mode: "fork", // Required for JARs
+      log_file: "./logs/service-event.log"
     },
     {
-      name   : "service-participant",
-      script : "java -jar ./service-registration/target/Participant-Service-1.0.0.jar",
-      log_file:  "./logs/service-participant.log",
-      instances: 2
+      name: "service-participant",
+      script: "java",
+      args: "-jar ./service-participant/target/Participant-Service-1.0.0.jar",
+      instances: 2,
+      exec_mode: "fork",
+      log_file: "./logs/service-participant.log"
     },
     {
-      name   : "service-registration",
-      script : "java -jar ./service-registration/target/Registration-Service-1.0.0.jar",
-      log_file:  "./logs/service-registration.log",
-      instances: 2
+      name: "service-registration",
+      script: "java",
+      args: "-jar ./service-registration/target/Registration-Service-1.0.0.jar",
+      instances: 2,
+      exec_mode: "fork",
+      log_file: "./logs/service-registration.log"
     }
   ]
 }
+
+// module.exports = {
+//   apps : [
+//     {
+//       name   : "service-event",
+//       script : "java -jar ./service-event/target/Event-Service-1.0.0.jar",
+//       log_file:  "./logs/service-event.log",
+//       instances: 2
+//     },
+//     {
+//       name   : "service-participant",
+//       script : "java -jar ./service-participant/target/Participant-Service-1.0.0.jar",
+//       log_file:  "./logs/service-participant.log",
+//       instances: 2
+//     },
+//     {
+//       name   : "service-registration",
+//       script : "java -jar ./service-registration/target/Registration-Service-1.0.0.jar",
+//       log_file:  "./logs/service-registration.log",
+//       instances: 2
+//     }
+//   ]
+// }
